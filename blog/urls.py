ce0c89d31ae12b090e34cbdd9604a5dbbc5708e9
Bilal <samibilal120@gmail.com>
2019-09-03 17:57:43 +0600

    Site

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import  path
-from blog.views import get_index
+from blog.views import *
 
 urlpatterns = [
     path('', get_index,name = 'index'),
+    path('<int:pk>/', post_detail, name = 'post_detail'),
+    path('new/',post_new,name= 'post_new'),
+    path('<int:pk>/edit/', post_edit, name='post_new'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+
+from .forms import PostForm
 from blog.models import Post
 
 def get_index(request):
     posts = Post.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
     return render (request, 'index.html' ,{'posts': posts})
 
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request,'post_details.html',{'post':post})
 
-def new_post(request):
-	pass
+def post_new(request):
+    form = PostForm
+    return render(request,'post_new.html',{'form' : form})
 
-def bilal(request):
-    pass
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'post_new.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'post_new.html', {'form': form})
